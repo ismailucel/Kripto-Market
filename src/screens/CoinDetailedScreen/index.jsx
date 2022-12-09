40649b352de40fa4709ff8a377a41539daa02539
İsmail Üçel <ismailucel@gmail.com>
2022-12-09 16:51:06 +0300

    implementing price tags

--- src/screens/CoinDetailedScreen/index.jsx
+++ src/screens/CoinDetailedScreen/index.jsx
@@ -2,17 +2,50 @@ import React from "react";
 import { View, Text } from "react-native" ;
 import Coin from '../../../assets/data/crypto.json'
 import CoinDetailedHeader from "./components/CoinDetailedHeader";
+import { AntDesign } from '@expo/vector-icons';
+import styles from './styles';
 
 const CoinDetailedScreen = () => {
-    const { image: { small }, name,symbol, market_data:{market_cap_rank},} = Coin;
+    const { 
+    image: { small }, 
+    name,
+    symbol,
+    market_data:
+    {
+      market_cap_rank,
+      current_price,
+      price_change_percentage_24h
+    },
+  } = Coin;
+
+     const percentageColor = price_change_percentage_24h < 0 ? '#ea3943': '#16c784'
+
+
     return(
-    <View>
+    <View style={{paddingHorizontal:10}}>
       <CoinDetailedHeader
        image ={small} 
        name = {name} 
        symbol ={symbol} 
        marketCapRank = {market_cap_rank}
        />
+       <View style = {styles.priceContainer}>
+        <View>
+          <Text style ={styles.name}>{name}</Text>
+          <Text style ={styles.currentPrice}>${current_price.usd}</Text>
+        </View>
+        <View style = {{backgroundColor:percentageColor, paddingHorizontal :3,paddingVertical:8,borderRadius:5,flexDirection:'row'}}>
+          <AntDesign 
+          name={price_change_percentage_24h < 0 ? 'caretdown' : 'caretup' } 
+          size={12} 
+          color = {'white'}
+           style = {{alignSelf : 'center',marginRight :10}} 
+           />
+          <Text style = {styles.priceChange}>
+          {price_change_percentage_24h.toFixed(2)}%
+        </Text>
+        </View>
+       </View>
     </View>
     );
 };
