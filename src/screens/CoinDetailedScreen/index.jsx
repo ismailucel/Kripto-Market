453c8a42b1aba1e0fac14cc667bbbe1a709dc36e
İsmail Üçel <ismailucel@gmail.com>
2022-12-09 13:22:11 +0300

    custom header and navigation changes

---
+++ src/screens/CoinDetailedScreen/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { View, Text } from "react-native" ;
+import Coin from '../../../assets/data/crypto.json'
+import CoinDetailedHeader from "./components/CoinDetailedHeader";
+
+const CoinDetailedScreen = () => {
+    const { image: { small }, name,symbol, market_data:{market_cap_rank},} = Coin;
+    return(
+    <View>
+      <CoinDetailedHeader
+       image ={small} 
+       name = {name} 
+       symbol ={symbol} 
+       marketCapRank = {market_cap_rank}
+       />
+    </View>
+    );
+};
+
+export default CoinDetailedScreen;
