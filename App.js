1a7f266834313684da74ca18af872ebbd6a630dc
İsmail Üçel <ismailucel@gmail.com>
2022-12-09 12:19:42 +0300

    using props for showing different coins

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, FlatList } from 'react-native';
 import CoinItem from './src/components/CoinItem';
+import cryptocurrencies from './assets/data/cryptocurrencies.json';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <CoinItem />
+      <FlatList 
+        data={cryptocurrencies}
+        renderItem = {({item}) => <CoinItem marketCoin={item} />}
+      />
       <StatusBar style="light" />
     </View>
   );
--- src/components/CoinItem/index.jsx
+++ src/components/CoinItem/index.jsx
@@ -3,27 +3,50 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
 import styles from './styles';
 
-const CoinItem = () => {
+const CoinItem = ({ marketCoin }) => {
+   const {
+      name,
+      current_price,
+      market_cap_rank,
+      price_change_percentage_24h, 
+      symbol, 
+      market_cap,
+      image
+   } = marketCoin;
+
+   const normalizeMarketCap = (marketCap) =>{
+      if(marketCap > 1_000_000_000_000){
+         return `${Math.floor(marketCap / 1_000_000_000_000)} T`
+      } if(marketCap > 1_000_000_000){
+         return `${Math.floor(marketCap / 1_000_000_000)} T`
+      } if(marketCap > 1_000_000){
+         return `${Math.floor(marketCap / 1_000_000)} T`
+      }if(marketCap > 1_000){
+         return `${Math.floor(marketCap / 1_000)} T`
+      }
+      return marketCap;
+   };
+
     return (
        <View style = {styles.coinContainer}>
          <Image
-            source={{uri:'https://assets.coingecko.com/coins/images/1/thumb/bitcoin.png?1547033579'}}
+            source={{uri:image}}
             style = {{height: 30, width: 30, marginRight:10,alignSelf: "center"}}
          /> 
         <View>
-             <Text style= {styles.title}>Bitcoin</Text>
+             <Text style= {styles.title}>{name}</Text>
             <View style = {{flexDirection: "row"}}>
             <View style = {styles.rankContainer}>
-                <Text style = {styles.rank}>1</Text>
+                <Text style = {styles.rank}>{market_cap_rank}</Text>
             </View>
-            <Text style = {styles.text}>BTC</Text>
+            <Text style = {styles.text}>{symbol.toUpperCase()}</Text>
             <AntDesign name='caretdown' size={12} color = "white" style = {{alignSelf : 'center',marginRight :10}} />
-            <Text style = {styles.text}>0.63%</Text>
+            <Text style = {styles.text}>{price_change_percentage_24h.toFixed(3)}%</Text>
          </View>
       </View>
-      <View style = {{marginLeft:'auto'}}>
-        <Text style= {styles.title}>56265.09</Text>
-        <Text style= {styles.text}>MCap 1.076 T</Text>
+      <View style = {{marginLeft:'auto', alignItems:'flex-end'}}>
+        <Text style= {styles.title}>{current_price}</Text>
+        <Text style= {{color:'white'}}>MCap {normalizeMarketCap(market_cap)}</Text>
       </View>
     </View>
     );
