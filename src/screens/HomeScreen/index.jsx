bff1869da2c3110f3b8685fcc5434d708df2c942
İsmail Üçel <ismailucel@gmail.com>
2022-12-11 17:48:55 +0300

    pagination

--- src/screens/HomeScreen/index.jsx
+++ src/screens/HomeScreen/index.jsx
@@ -9,14 +9,20 @@ const HomeScreen = () => {
   const [coins, setCoins] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  const fetchCoins = async () => {
+  const fetchCoins = async (pageNumber) => {
+    if(loading){
+      return;
+    }
     setLoading(true);
-    const coinsData = await getMarketData();
-    setCoins(coinsData);
+    const coinsData = await getMarketData(pageNumber);
+    setCoins((existingCoins) => ([...existingCoins, ...coinsData]));
     setLoading(false);
   }
 
   const refetchCoins = async() => {
+    if(loading){
+      return;
+    }
      setLoading(true);
     const coinsData = await getMarketData();
     setCoins(coinsData);
@@ -32,6 +38,7 @@ const HomeScreen = () => {
         <FlatList 
         data={coins}
         renderItem = {({item}) => <CoinItem marketCoin={item} />}
+        onEndReached={()=> fetchCoins((coins.length / 50) + 1 )}
         refreshControl={
           <RefreshControl 
            refreshing={loading}
