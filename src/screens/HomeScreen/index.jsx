dc7bf30ebc59defe812b829742667f757a9815bc
İsmail Üçel <ismailucel@gmail.com>
2022-12-11 17:15:30 +0300

    replacing from dummy data to real data

--- src/screens/HomeScreen/index.jsx
+++ src/screens/HomeScreen/index.jsx
@@ -1,14 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CoinItem from '../../components/CoinItem';
-import cryptocurrencies from '../../../assets/data/cryptocurrencies.json';
-import { FlatList } from 'react-native';
+//import cryptocurrencies from '../../../assets/data/cryptocurrencies.json';
+import { getMarketData } from '../../services/requests'
+import { FlatList, RefreshControl } from 'react-native';
 
 
 const HomeScreen = () => {
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchCoins = async () => {
+    setLoading(true);
+    const coinsData = await getMarketData();
+    setCoins(coinsData);
+    setLoading(false);
+  }
+
+  const refetchCoins = async() => {
+     setLoading(true);
+    const coinsData = await getMarketData();
+    setCoins(coinsData);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchCoins()
+  }, [])
+  
+  
     return(
         <FlatList 
-        data={cryptocurrencies}
+        data={coins}
         renderItem = {({item}) => <CoinItem marketCoin={item} />}
+        refreshControl={
+          <RefreshControl 
+           refreshing={loading}
+           tintColor='white'
+           onRefresh={refetchCoins}
+          /> 
+        }
       />
     );
 };
