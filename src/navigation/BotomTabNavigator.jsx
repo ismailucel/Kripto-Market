8aa3fea5d8b4a342cf171388c0cba066a9e16af4
İsmail Üçel <ismailucel@gmail.com>
2022-12-11 16:48:29 +0300

    watchlist screen created

--- src/navigation/BotomTabNavigator.jsx
+++ src/navigation/BotomTabNavigator.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from "../screens/HomeScreen";
+import WatchlistScreen from "../screens/WatchlistScreen";
 import { AntDesign } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
@@ -18,9 +20,25 @@ const BottomTabNavigator = () => {
             backgroundColor: '#181818'
         }
     }}>
-      <Tab.Screen name="Home" component={HomeScreen} options= {{
-        tabBarIcon : ({focused, color}) => (<AntDesign name="home" size={focused ? 30 : 25} color={color} />)
-      }} />
+      <Tab.Screen 
+      name="Home" 
+      component={HomeScreen} 
+      options= {{
+        tabBarIcon : ({focused, color}) => (
+        <AntDesign name="home" size={focused ? 30 : 25} color={color} />
+        ),
+      }} 
+      />
+      <Tab.Screen
+        name="Watchlist"
+        component={WatchlistScreen}
+        options= {{
+         tabBarIcon : ({focused, color}) => (
+         <FontAwesome name="star" size={focused ? 30 : 25} color={color}/>
+        ),
+      }}      
+      /> 
+
      </Tab.Navigator>
   );
 };
---
+++ src/screens/WatchlistScreen/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from 'react-native'
+
+const WatchlistScreen = () =>{
+    return(
+        <View>
+            <Text style = {{color:'white'}}> Hello Watchlist</Text>
+        </View>
+    )
+};
+
+export default WatchlistScreen;
