bcbfbf6964a5bdf8606f3c7e502821e9da8ba66e
İsmail Üçel <ismailucel@gmail.com>
2022-12-11 15:49:07 +0300

    BottomTabNavigator implementation

---
+++ src/navigation/BotomTabNavigator.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from "../screens/HomeScreen";
+import { AntDesign } from '@expo/vector-icons';
+
+const Tab = createBottomTabNavigator();
+
+
+const BottomTabNavigator = () => {
+ return (
+    <Tab.Navigator 
+    initialRouteName="Home"
+    screenOptions={{
+        headerShown:false,
+        tabBarActiveTintColor:'white',
+        tabBarInactiveTintColor:'grey',
+        tabBarStyle:{
+            backgroundColor: '#181818'
+        }
+    }}>
+      <Tab.Screen name="Home" component={HomeScreen} options= {{
+        tabBarIcon : ({focused, color}) => (<AntDesign name="home" size={focused ? 30 : 25} color={color} />)
+      }} />
+     </Tab.Navigator>
+  );
+};
+
+export default BottomTabNavigator;
