04158858baa15389af3eeeae6184edf83cdf17f0
İsmail Üçel <ismailucel@gmail.com>
2022-12-13 11:26:23 +0300

    designing portfolio

--- src/screens/PortfolioScreen/components/PortfolioAssetsList/index.jsx
+++ src/screens/PortfolioScreen/components/PortfolioAssetsList/index.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { View, Text, FlatList } from "react-native";
 import { AntDesign } from '@expo/vector-icons';
+import styles from "./styles";
+
 const PortfolioAssetsList = () =>{
     return(
         <View>
@@ -9,17 +11,24 @@ const PortfolioAssetsList = () =>{
             renderItem={()=> <Text>Item</Text>}
             ListHeaderComponent={
                 <>
-                    <Text>Current Balance</Text>
-                    <Text>20000 TRY</Text>
-                    <Text>1000 (All Time)</Text>
+                    <View style={styles.balanceContainer}>
+                        <View>
+                             <Text style={styles.currentBalance}>Current Balance</Text>
+                            <Text style={styles.currentBalanceValue}>20000 TRY</Text>
+                            <Text style={styles.valueChange}>1000 TRY (All Time)</Text>
+                        </View>
+                        <View style={styles.priceChangePercentageContainer}>
+                            <AntDesign 
+                                name={'caretup'} 
+                                size={12} 
+                                color = {'white'}
+                                style = {{alignSelf : 'center',marginRight :10}} 
+                             />
+                            <Text style={styles.percentageChange}>1.2%</Text>
+                        </View>
+                    </View>
                     <View>
-                        <AntDesign 
-                            name={'caretup'} 
-                            size={12} 
-                            color = {'white'}
-                            style = {{alignSelf : 'center',marginRight :10}} 
-                         />
-                        <Text>1.2%</Text>
+                        <Text style={styles.assetsLabel}> Your assets</Text>
                     </View>
                 </>
             }
---
+++ src/screens/PortfolioScreen/components/PortfolioAssetsList/styles.js
@@ -0,0 +1,50 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    currentBalance:{
+        color:'white',
+        fontWeight: '600',
+        fontSize:15
+    },
+    currentBalanceValue:{
+        color:'white',
+        fontSize:40,
+        fontWeight:'700',
+        letterSpacing:1
+    },
+    valueChange:{
+        fontWeight:'600',
+        fontSize:16,
+        color:'#16c784' 
+    },
+    percentageChange:{
+        color:'white',
+        fontWeight:'500',
+        fontSize:17
+    },
+    balanceContainer:{
+        flexDirection:'row',
+        justifyContent:'space-between',
+        alignItems:'center',
+        marginTop:10,
+        marginBottom:5,
+        marginHorizontal:10
+    },
+    priceChangePercentageContainer:{
+        flexDirection:'row',
+        backgroundColor:'#16c784',
+        paddingHorizontal:3,
+        paddingVertical:8,
+        borderRadius:5
+    },
+    assetsLabel:{
+        color:'white',
+        fontSize:23,
+        fontWeight:'700',
+        paddingVertical:2,
+        paddingHorizontal:10
+    }
+
+})
+
+export default styles;
