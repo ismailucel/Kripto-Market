5aafd4c9cdd7e98ac767c437ff34c2ebb1f585cc
İsmail Üçel <ismailucel@gmail.com>
2022-12-13 11:54:28 +0300

    implementing portfolio asset item

---
+++ src/screens/PortfolioScreen/components/PortfolioAssetItem/index.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { View, Text, Image } from 'react-native'
+import styles from "./styles";
+import { AntDesign } from '@expo/vector-icons';
+
+const PortfolioAssetItem=() =>{
+    return(
+        <View style={styles.coinContainer}>
+            <Image source={{uri:""}} style={{height:30,width:30}}/>
+            <View>
+               <Text style={styles.title}>Bitcoin</Text> 
+               <Text style={styles.ticker}>BTC</Text>
+            </View>
+            <View style={{marginLeft:'auto'}}>
+                <Text style={styles.title}>8000 TRY</Text>
+                <View style={{flexDirection:'row'}}>
+                    <AntDesign 
+                            name={'caretup'} 
+                            size={12} 
+                            color = {'#16c784'}
+                            style = {{alignSelf : 'center',marginRight :10}} 
+                    />
+                    <Text style={{color:'#16c784',fontWeight:'700'}}>1.2%</Text>
+                </View>
+            </View>
+            <View style={styles.quantityContainer}>
+                <Text style={styles.title}>16000 TRY</Text>
+                <Text style={styles.ticker}>2 BTC</Text>
+            </View>
+        </View>
+    )
+};
+
+export default PortfolioAssetItem;
