f9efa9609dca8f29e719eb8d6f8df1e6c3997da8
İsmail Üçel <ismailucel@gmail.com>
2022-12-13 14:17:42 +0300

    implementing searchable dropdown for asset screen

--- src/screens/AddNewAssetScreen/index.jsx
+++ src/screens/AddNewAssetScreen/index.jsx
@@ -1,10 +1,54 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, Text, TextInput, Pressable } from "react-native";
+import SearchableDropDown from "react-native-searchable-dropdown";
+import styles from "./styles";
 
 const AddNewAssetScreen = () =>{
+    const [boughtAssetQuantity, setBoughtAssetQuantity] = useState("")
     return(
-        <View>
-            <Text style ={{color:'white'}}>hello</Text>
+        <View style={{flex:1}}>
+            <SearchableDropDown
+                items={[]}
+                onItemSelect={(item) => console.log(item)}
+                containerStyle={styles.dropdownContainer}
+                itemStyle={styles.item}
+                itemTextStyle={{color:'white'}}
+                resetValue = {false}
+                placeholder={"Select a coin..."}
+                placeholderTextColor='white'
+                textInputProps={{
+                    underlineColorAndroid:'transparent',
+                    style:{
+                        padding:12,
+                        borderWidth:1.5,
+                        borderColor:'#444444',
+                        borderRadius:5,
+                        backgroundColor:'#1e1e1e',
+                        color:'white'
+                    }
+                }}
+
+            />
+            <View style={styles.boughtQuantityContainer}>
+              <View style={{flexDirection:'row'}}>
+                <TextInput 
+                    style={{
+                        color:'white',
+                        fontSize:90
+                    }}
+                    value={boughtAssetQuantity} 
+                    placeholder='0' 
+                    keyboardType="numeric"
+                    onChangeText={setBoughtAssetQuantity}
+                    />
+                <Text style={styles.ticker}>BTC</Text>
+              </View> 
+                <Text style={styles.pricePerCoin}>10000 TRY per coin</Text>
+            </View>
+            
+            <Pressable style={styles.buttonContainer} onPress={() => navigation.navigate("AddNewAssetScreen")}>
+                  <Text style={styles.buttonText}> Add new asset</Text>
+            </Pressable>
         </View>
     )
 }
