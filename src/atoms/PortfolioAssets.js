fc7e650860855d452ed92549b12ba4153da2484f
İsmail Üçel <ismailucel@gmail.com>
2022-12-15 13:50:41 +0300

    assets&services for get prices from localstorage

--- src/atoms/PortfolioAssets.js
+++ src/atoms/PortfolioAssets.js
@@ -4,30 +4,29 @@ import { getWatchlistedCoins } from '../services/requests';
 
 export const allPortfolioBoughtAssets = selector({
     key:'allPortfolioBoughtAssets',
-    get: async () =>{
+    get:async () => {
         const jsonValue = await AsyncStorage.getItem('@portfolio_coins')
-        return jsonValue !=null ? JSON.parse(jsonValue): [];
+        return jsonValue != null ? JSON.parse(jsonValue) : []
     }
 })
 
-export const allPortfolioBoughtAssetsFromAPI= selector({
+export const allPortfolioBoughtAssetsFromAPI = selector({
     key:'allPortfolioBoughtAssetsFromAPI',
-    get: async ({get}) =>{
-        const boughtPortfolioAssets= get(allPortfolioBoughtAssetsInStorage)
+    get:async ({get})=>{
+        const boughtPortfolioAssets = get(allPortfolioBoughtAssetsInStorage)
         const portfolioAssetsMarketData = await getWatchlistedCoins(1, boughtPortfolioAssets.map((portfolioAsset) => portfolioAsset.id).join(','))
 
-        const boughtAssets = boughtPortfolioAssets.map((boughtAsset) => {
-            const portfolioAsset= portfolioAssetsMarketData.filter((item)=>boughtAsset.id===item.id)
-            return{
+        const boughtAssets = boughtPortfolioAssets.map((boughtAsset) =>{
+            const portfolioAsset =portfolioAssetsMarketData.filter((item) => boughtAsset.id ===item.id)[0]
+            return {
                 ...boughtAsset,
                 currentPrice: portfolioAsset.current_price,
-                priceChangePercentage:portfolioAsset.price_chane_percentage_24
+                priceChangePercentage: portfolioAsset.price_change_percentage_24h
             }
         })
-        return boughtAssets.sort((item1, item2) => (item1.quantityBought*item1.currentPrice)<(item2.quantityBought*item2.c))
+        return boughtAssets.sort((item1,item2) => (item1.quantityBought * item1.currentPrice) < (item2.quantityBought * item2.currentPrice))
     }
 })
-
 export const allPortfolioAssets = atom({
     key:'allPortfolioAssets',
     default:allPortfolioBoughtAssetsFromAPI
